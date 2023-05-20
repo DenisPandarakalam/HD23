6396312b47f22b41142815b33c9a7d0e63512d57
aarush65 <aarush.agrawal@gmail.com>
2023-05-20 15:50:09 -0700

    Co-authored-by: Lucas <lucaspunz@users.noreply.github.com>

--- frontend/app/api/ai/route.ts
+++ frontend/app/api/ai/route.ts
@@ -1,5 +1,25 @@
 import { NextResponse } from "next/server";
-
+import openai from "@/app/utils/openai";
 export async function GET(request: Request) {
   return NextResponse.json({ message: "Hello from the API" });
+}
+
+type PromptData = {
+  prompt: string;
+};
+
+export async function POST(request: PromptData) {
+  "use server";
+  const completion = await openai.createCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content: "What is 2 + 2",
+      },
+    ],
+  });
+  // const res = completion.data.choices[0].message.content;
+  console.log("res:", completion.data);
+  return NextResponse.json({ message: "0" });
 }
